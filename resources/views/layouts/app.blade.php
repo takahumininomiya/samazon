4bae8408a7dba443894935bd7df10dd395d42bd7
EC2 Default User <lotte04715923210@gmail.com>
2021-11-05 07:40:01 +0000

    08 add footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -10,24 +11,28 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ secure_asset('js/app.js') }}" defer></script>
-    <script src="https://kit.fontawesome.com/513d952c93.js" crossorigin="anonymous"></script>
-    <script src="https://kit.fontawesome.com/XXXXXXXXXX.js" crossorigin="anonymous"></script>
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
- <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
-   <link href="{{ secure_asset('css/samazon.css')}}" rel="stylesheet">   
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
+
 <body>
     <div id="app">
         @component('components.header')
         @endcomponent
-        <main class="py-4 mb-5">
+        
+        <main class="py-4">
             @yield('content')
         </main>
+
+        @component('components.footer')
+        @endcomponent
     </div>
 </body>
+
 </html>
